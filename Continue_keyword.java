862bdf29b463916782f583cb9b2468cf475fc2f8
Your Name <you@example.com>
2024-10-09 21:59:17 +0530

    new file pushingdevelop

---
+++ Continue_keyword.java
@@ -0,0 +1,23 @@
+package Class_13;
+
+public class Continue_keyword {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=100; i++) {
+			
+				if(i==99) {
+					
+					continue;
+				}
+			
+			
+			System.out.println(i);
+		}
+		
+		
+		
+
+	}
+
+}
